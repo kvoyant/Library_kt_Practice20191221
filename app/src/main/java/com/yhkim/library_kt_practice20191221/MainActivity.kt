09d4859c1e083c580a28e5496f51bc48f6614743
kvoyant <kvoyant@gmail.com>
2019-12-21 13:30:28 +0900

    Initial commit

---
+++ app/src/main/java/com/yhkim/library_kt_practice20191221/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.yhkim.library_kt_practice20191221
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+        Glide.with(mContext).load("https://m1.daumcdn.net/cfile297/image/990F2A425D0A96F10A7E54").into(profileImage)
+//        Glide.with(mContext).load("https://m1.daumcdn.net/cfile265/image/996BF54A5A9F2D802FB722").into(profileImage)
+    }
+
+}
